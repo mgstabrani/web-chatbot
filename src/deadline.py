b2897af17b034abf0fbd59dc61f9cc014e18fa90
mgstabrani <mgstabrani19@gmail.com>
2021-04-26 20:48:53 +0700

    Add display deadline feature

---
+++ src/deadline.py
@@ -0,0 +1,5 @@
+deadline = [
+    ["22/08/2021","IF2240","Tubes","String Matching"],
+    ["21/07/2021","IF2230","Tucil","Normalisasi"],
+    ["22/05/2021","IF2230","Tubes","Index Tuning"]
+]
--- src/feature.py
+++ src/feature.py
@@ -1,11 +1,13 @@
 import matcher
 import re
+from deadline import *
 
 # mungkin kedepannya perlu dipisah si kata2 pentingnya
 # kayak ada kata penting untuk jenis tugas,
 # ada kata penting untuk jenis pertanyaan, dll
 kata_penting = ["deadline", "tubes", "tucil", "kuis", "ujian", "pr"]
 kata_help = ["bisa","lakukan","help","command","fitur"]
+kata_tampil_deadline = ["when","deadline","kapan"]
 fitur = [
     "Menambahkan task baru",
     "Melihat daftar task",
@@ -14,6 +16,19 @@ fitur = [
     "Menandai bahwa suatu task sudah dikerjakan",
     "Memberikan rekomendasi kata"
     ]
+
+def tampilDeadline(usrMsg):
+    for i in range(len(deadline)):
+        #General untuk tugas
+        if(matcher.match(usrMsg,"tugas")):
+            if(matcher.match(usrMsg,deadline[i][1].lower())):
+                return deadline[i][0]
+        #Spesifik, tucil, tubes atau pr
+        else:
+            if(matcher.match(usrMsg,deadline[i][1].lower()) and matcher.match(usrMsg,deadline[i][2].lower())):
+                return deadline[i][0]
+
+    return "Tidak ada deadline itu"
 
 def help():
     output = "[Fitur]<br>"
@@ -32,6 +47,11 @@ def process(usrMsg):
     for pattern in kata_help:
         if (matcher.match(text, pattern)) :
             return help()
+
+    #Menampilkan tanggal deadline suatu task
+    for pattern in kata_tampil_deadline:
+        if (matcher.match(text, pattern)) :
+            return tampilDeadline(text)
 
     for pattern in kata_penting:
         if (matcher.match(text, pattern)) :
